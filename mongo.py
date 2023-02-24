2684915c57bd1aa01ead043fa2542775836a50f7
julipreciado <juliana.preciado1@udea.edu.co>
2023-02-24 11:46:26 -0500

    up

---
+++ mongo.py
@@ -0,0 +1,24 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://julipreciado:1234@cluster0.p75sqrz.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+mydb=client["bbdd"]
+mycol=mydb["clientes"]
+
+mydict = {"nombre": "Maria", "direccion": 123}
+x=mycol.insert_one(mydict)
+print(x.inserted_id)
+
+#for x in mycol.find({"direccion":1}):
+#    print(x)
+
+for y in mycol.find():
+    print(y)
+
+newvalues={"$set": {"nombre":"Luis"}}
+
+mycol.update_one(mydict, newvalues)
+
+#print "clientes" después del update:
+for x in mycol.find():
+    print(x)
